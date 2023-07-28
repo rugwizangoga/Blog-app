f89369ae89b20c270f9489e85cbe484d352999c1
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-07-28 14:21:43 +0200

    implement the suggested changes by the reviewer

--- db/migrate/20230728064539_create_posts.rb
+++ db/migrate/20230728064539_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.references :Author, null: false, foreign_key: { to_table: :users }, index: true
       t.string :Title
-      t.string :Text
+      t.text :Text
       t.integer :CommentsCounter
       t.integer :LikesCounter
 
