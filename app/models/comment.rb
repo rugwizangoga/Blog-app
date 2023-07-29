47476232b9ae0758e61dce16ed02c6f6a2ded0f1
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-07-29 11:17:22 +0200

    process data

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :User
-  belongs_to :Post
+  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- db/migrate/20230728064539_create_posts.rb
+++ db/migrate/20230728064539_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :Author, null: false, foreign_key: { to_table: :users }, index: true
-      t.string :Title
-      t.text :Text
-      t.integer :CommentsCounter
-      t.integer :LikesCounter
+      t.references :author, null: false, foreign_key: { to_table: :users }, index: true
+      t.string :title
+      t.text :text
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
 
       t.timestamps
     end
